d76ffa40011e3bb07edbd485d6098a9a737b4b4c
ClimbingAl <aristotlestarteditall@gmail.com>
2023-08-01 10:36:08 +0000

    Created output file directories

--- thing/thing/src/main/java/patterns/hqdm/ThingApp.java
+++ thing/thing/src/main/java/patterns/hqdm/ThingApp.java
@@ -1,11 +1,14 @@
 package patterns.hqdm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.List;
 
 import uk.gov.gchq.magmacore.hqdm.rdf.iri.HQDM;
 import uk.gov.gchq.magmacore.hqdm.rdf.iri.IriBase;
-import uk.gov.gchq.magmacore.hqdm.rdf.util.Pair;
 import uk.gov.gchq.magmacore.service.MagmaCoreService;
 import uk.gov.gchq.magmacore.service.MagmaCoreServiceFactory;
 
@@ -17,8 +20,6 @@ public class ThingApp
 {
     public static void main( String[] args )
     {
-        final PrintStream stream_out = System.out;
-
         // Create map of prefixes
         final List<IriBase> listPrefixes = List.of(
                 PatternsUtils.PATTERNS_BASE,
@@ -33,10 +34,22 @@ public class ThingApp
 
         ThingExample.createAndAddThing(service);
 
-        System.out.println("Data generated in TTL:\n");
-        service.exportTtl(stream_out);
-        System.out.println("\nData generated as statements:\n");
-        service.exportStatements(stream_out);
+        System.out.println("Data generated in TTL in example-files/thing.ttl.\n");
+        try {
+            final PrintStream ttl_stream_out = new PrintStream("example-files/thing.ttl");
+            final PrintStream stmt_stream_out = new PrintStream("example-files/thing.stmt");
+
+            service.exportTtl(ttl_stream_out);
+            ttl_stream_out.close();
+
+            System.out.println("Data generated as statements in example-files/thing.stmt.\n");
+            service.exportStatements(stmt_stream_out);
+            stmt_stream_out.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("thing example write: " + e);
+        } catch (IOException e) {
+            System.err.println("thing example write: " + e);
+        }
 
         System.out.println("\nDone\n");
 
